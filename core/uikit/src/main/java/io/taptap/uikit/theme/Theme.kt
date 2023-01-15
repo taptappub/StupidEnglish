dc80c27cd993269f2dd2886191cef49b20b19736
a.potapov <a.potapov@unlimint.com>
2023-01-15 23:29:46 +0300

    libs update

--- core/uikit/src/main/java/io/taptap/uikit/theme/Theme.kt
+++ core/uikit/src/main/java/io/taptap/uikit/theme/Theme.kt
@@ -1,6 +1,5 @@
 package io.taptap.uikit.theme
 
-import android.content.Context
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -14,15 +13,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import io.taptap.uikit.prefs.ThemeType
-import io.taptap.uikit.prefs.UiKitSettings
 
 private val DarkColorPalette = darkColorScheme(
     surface = Esspresso,
