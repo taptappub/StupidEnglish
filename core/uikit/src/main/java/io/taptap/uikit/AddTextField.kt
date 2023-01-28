62ec4f5f3ab57d81403d9c7025ad0107996d69ad
a.potapov <a.potapov@unlimint.com>
2023-01-28 16:03:28 +0300

    new tasks

--- core/uikit/src/main/java/io/taptap/uikit/AddTextField.kt
+++ core/uikit/src/main/java/io/taptap/uikit/AddTextField.kt
@@ -9,6 +9,8 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
