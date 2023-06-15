e818da1dd990028f7d0c964372c17b729b621a4f
abusik <and2919@gmail.com>
2023-06-15 17:21:04 +0300

    Добавил превью для MainList

--- app/src/main/java/io/taptap/stupidenglish/features/groupdetails/ui/GroupDetailsScreen.kt
+++ app/src/main/java/io/taptap/stupidenglish/features/groupdetails/ui/GroupDetailsScreen.kt
@@ -25,10 +25,13 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.taptap.stupidenglish.R
 import io.taptap.stupidenglish.base.LAUNCH_LISTEN_FOR_EFFECTS
+import io.taptap.stupidenglish.base.model.Word
 import io.taptap.stupidenglish.base.model.WordWithGroups
+import io.taptap.stupidenglish.features.groupdetails.ui.GroupDetailsContract.*
 import io.taptap.stupidenglish.features.groupdetails.ui.model.GroupDetailsButtonUI
 import io.taptap.stupidenglish.features.groupdetails.ui.model.GroupDetailsDynamicTitleUI
 import io.taptap.stupidenglish.features.groupdetails.ui.model.GroupDetailsEmptyUI
@@ -43,6 +46,7 @@ import io.taptap.uikit.StupidEnglishTopAppBar
 import io.taptap.uikit.complex.WordItemRow
 import io.taptap.uikit.fab.BOTTOM_BAR_MARGIN
 import io.taptap.uikit.group.getTitle
+import io.taptap.uikit.theme.StupidEnglishTheme
 import io.taptap.uikit.theme.StupidLanguageBackgroundBox
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
@@ -54,11 +58,11 @@ import kotlinx.coroutines.flow.onEach
 @Composable
 fun GroupDetailsScreen(
     context: Context,
-    state: GroupDetailsContract.State,
+    state: State,
     mainList: List<GroupDetailsUIModel>,
-    effectFlow: Flow<GroupDetailsContract.Effect>?,
-    onEventSent: (event: GroupDetailsContract.Event) -> Unit,
-    onNavigationRequested: (navigationEffect: GroupDetailsContract.Effect.Navigation) -> Unit
+    effectFlow: Flow<Effect>?,
+    onEventSent: (event: Event) -> Unit,
+    onNavigationRequested: (navigationEffect: Effect.Navigation) -> Unit
 ) {
     val scaffoldState: ScaffoldState = rememberScaffoldState()
 
@@ -66,38 +70,34 @@ fun GroupDetailsScreen(
     LaunchedEffect(LAUNCH_LISTEN_FOR_EFFECTS) {
         effectFlow?.onEach { effect ->
             when (effect) {
-                is GroupDetailsContract.Effect.GetWordsError ->
+                is Effect.GetWordsError ->
                     scaffoldState.snackbarHostState.showSnackbar(
                         message = context.getString(effect.errorRes),
                         duration = SnackbarDuration.Short
                     )
-                is GroupDetailsContract.Effect.ShowRecover -> {
+
+                is Effect.ShowRecover -> {
                     val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
                         message = context.getString(R.string.grdt_delete_message),
                         duration = SnackbarDuration.Short,
                         actionLabel = context.getString(R.string.grdt_recover)
                     )
                     when (snackbarResult) {
-                        SnackbarResult.Dismissed -> onEventSent(GroupDetailsContract.Event.OnApplyDismiss)
-                        SnackbarResult.ActionPerformed -> onEventSent(GroupDetailsContract.Event.OnRecover)
+                        SnackbarResult.Dismissed -> onEventSent(Event.OnApplyDismiss)
+                        SnackbarResult.ActionPerformed -> onEventSent(Event.OnRecover)
                     }
                 }
-                is GroupDetailsContract.Effect.Navigation.BackTo -> onNavigationRequested(effect)
-                is GroupDetailsContract.Effect.Navigation.ToAddSentence -> onNavigationRequested(
-                    effect
-                )
-                is GroupDetailsContract.Effect.Navigation.ToAddWordWithGroup -> onNavigationRequested(
-                    effect
-                )
-                is GroupDetailsContract.Effect.Navigation.ToFlashCards -> onNavigationRequested(
-                    effect
-                )
+
+                is Effect.Navigation.BackTo -> onNavigationRequested(effect)
+                is Effect.Navigation.ToAddSentence -> onNavigationRequested(effect)
+                is Effect.Navigation.ToAddWordWithGroup -> onNavigationRequested(effect)
+                is Effect.Navigation.ToFlashCards -> onNavigationRequested(effect)
             }
         }?.collect()
     }
 
     BackHandler {
-        onEventSent(GroupDetailsContract.Event.OnBackClick)
+        onEventSent(Event.OnBackClick)
     }
 
     StupidEnglishScaffold(
@@ -114,15 +114,15 @@ fun GroupDetailsScreen(
 @OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun ContentScreen(
-    state: GroupDetailsContract.State,
+    state: State,
     mainList: List<GroupDetailsUIModel>,
-    onEventSent: (event: GroupDetailsContract.Event) -> Unit
+    onEventSent: (event: Event) -> Unit
 ) {
     StupidLanguageBackgroundBox(
         topbar = {
             StupidEnglishTopAppBar(
                 text = "",
-                onNavigationClick = { onEventSent(GroupDetailsContract.Event.OnBackClick) },
+                onNavigationClick = { onEventSent(Event.OnBackClick) },
             )
         }
     ) {
@@ -147,7 +147,7 @@ fun MainList(
     groupItems: List<GroupDetailsUIModel>,
     deletedWords: List<WordWithGroups>,
     listState: LazyListState,
-    onEventSent: (event: GroupDetailsContract.Event) -> Unit
+    onEventSent: (event: Event) -> Unit
 ) {
     LazyColumn(
         state = listState,
@@ -166,13 +166,13 @@ fun MainList(
                 if (dismissState.currentValue != DismissValue.Default) {
                     LaunchedEffect(Unit) {
                         dismissState.reset()
-                        onEventSent(GroupDetailsContract.Event.OnRecovered(item as GroupDetailsWordItemUI))
+                        onEventSent(Event.OnRecovered(item as GroupDetailsWordItemUI))
                     }
                 }
             } else {
                 if (item is GroupDetailsWordItemUI) {
                     if (dismissState.isDismissed(DismissDirection.StartToEnd)) {
-                        onEventSent(GroupDetailsContract.Event.OnDismiss(item))
+                        onEventSent(Event.OnDismiss(item))
                     }
                 }
             }
@@ -182,22 +182,25 @@ fun MainList(
                     text = stringResource(id = item.valueRes),
                     onClick = {
                         when (item.buttonId) {
-                            GroupDetailsContract.ButtonId.remove -> onEventSent(GroupDetailsContract.Event.OnRemoveGroupClick)
-                            GroupDetailsContract.ButtonId.flashcards -> onEventSent(GroupDetailsContract.Event.ToFlashCards)
-                            GroupDetailsContract.ButtonId.learn -> onEventSent(GroupDetailsContract.Event.ToAddSentence)
-                            GroupDetailsContract.ButtonId.addWord -> onEventSent(GroupDetailsContract.Event.OnAddWordClick)
+                            ButtonId.remove -> onEventSent(Event.OnRemoveGroupClick)
+                            ButtonId.flashcards -> onEventSent(Event.ToFlashCards)
+                            ButtonId.learn -> onEventSent(Event.ToAddSentence)
+                            ButtonId.addWord -> onEventSent(Event.OnAddWordClick)
                         }
                     }
                 )
+
                 is GroupDetailsDynamicTitleUI -> AverageTitle(
                     text = item.currentGroup.getTitle(),
                     modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)
                 )
+
                 is GroupDetailsEmptyUI -> EmptyListContent(
                     title = stringResource(id = R.string.word_empty_list_title),
                     description = stringResource(id = item.descriptionRes),
                     modifier = Modifier.height(300.dp)
                 )
+
                 is GroupDetailsWordItemUI -> WordItemRow(
                     word = item.word,
                     description = item.description,
@@ -207,5 +210,39 @@ fun MainList(
                 )
             }
         }
+    }
+}
+
+
+@Preview
+@ExperimentalMaterialApi
+@ExperimentalFoundationApi
+@Composable
+fun MainListPreview() {
+    StupidEnglishTheme {
+        MainList(
+            groupItems = listOf(
+                GroupDetailsDynamicTitleUI(GroupItemUI(0, "Group name")),
+                //
+                GroupDetailsButtonUI(ButtonId.addWord, R.string.grdt_add_word),
+                GroupDetailsButtonUI(ButtonId.learn, R.string.grdt_learn),
+                GroupDetailsButtonUI(ButtonId.remove, R.string.grdt_remove),
+                GroupDetailsButtonUI(ButtonId.flashcards, R.string.grdt_flashcards),
+                GroupDetailsButtonUI(ButtonId.share, R.string.grdt_share),
+                //
+                GroupDetailsWordItemUI(id = 0, word = "Word", description = "Description"),
+                GroupDetailsWordItemUI(id = 1, word = "Word", description = "Description"),
+                GroupDetailsWordItemUI(id = 2, word = "Word", description = "Description"),
+                GroupDetailsWordItemUI(id = 3, word = "Word", description = "Description"),
+            ),
+            deletedWords = listOf(
+                WordWithGroups(
+                    word = Word(4, "Deleted word", "desciption", 0),
+                    groups = listOf()
+                )
+            ),
+            listState = rememberLazyListState(),
+            onEventSent = {},
+        )
     }
 }
